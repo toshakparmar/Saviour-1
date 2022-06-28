6b9a60dab47d34bbbc86b055847ad3a2337aacf1
sajal12092001 <105692920+sajal12092001@users.noreply.github.com>
2022-06-28 19:45:16 +0530

    SplashScreen

---
+++ SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.saviour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(() -> {
+
+            startActivity(new Intent(SplashScreen.this,Login.class));
+            finish();
+        },2000);
+    }
+}
